927bd303a20ecca4502f49e177d4f237ac512d57
Amir <amirsanni@gmail.com>
2016-12-23 02:58:31 +0100

    Config.js created

---
+++ js/config.js
@@ -0,0 +1,36 @@
+/**
+ * @author Amir Sanni <amirsanni@gmail.com>
+ * @date 23-Dec-2016
+ */
+
+'use strict';
+
+const appRoot = setAppRoot('webrtc-ratchet-chat-app', '');
+const spinnerClass = 'fa fa-spinner faa-spin animated';
+
+
+function setAppRoot(devFolderName, prodFolderName){
+    var hostname = window.location.hostname;
+
+    /*
+     * set the appRoot
+     * This will work for both http, https with or without www
+     * @type String
+     */
+    
+    //attach trailing slash to both foldernames
+    var devFolder = devFolderName ? devFolderName+"/" : "";
+    var prodFolder = prodFolderName ? prodFolderName+"/" : "";
+    
+    var baseURL = "";
+    
+    if(hostname === "localhost" || (hostname.search("192.168.") !== -1)  || (hostname.search("127.0.0.") !== -1)){
+        baseURL = window.location.origin+"/"+devFolder;
+    }
+    
+    else{
+        baseURL = window.location.origin+"/"+prodFolder;
+    }
+    
+    return baseURL;
+}
