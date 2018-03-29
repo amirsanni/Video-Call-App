2cfb59388586527f15d9daf067f983f76a493f0e
sohail0992 <msohail.se@gmail.com>
2018-03-29 13:27:25 +0500

    dynamic path added to config.js and deprecated window.createObjecUrl removed

--- js/config.js
+++ js/config.js
@@ -5,9 +5,19 @@
 
 'use strict';
 
-
-const appRoot = setAppRoot('video-call-app', 'video-call-app');
+var app = getRootWebSitePath();
+const appRoot = setAppRoot(app,app);
 const spinnerClass = 'fa fa-spinner faa-spin animated';
+
+function getRootWebSitePath()
+{
+    var _location = document.location.toString();
+    var applicationNameIndex = _location.indexOf('/', _location.indexOf('://') + 3);
+    var applicationName = _location.substring(0, applicationNameIndex) + '/';
+    var webFolderIndex = _location.indexOf('/', _location.indexOf(applicationName) + applicationName.length);
+    var webFolderFullPath = _location.substring(0, webFolderIndex);
+    return webFolderFullPath;
+}
 
 function setAppRoot(devFolderName, prodFolderName){
     var hostname = window.location.hostname;
@@ -17,16 +27,16 @@ function setAppRoot(devFolderName, prodFolderName){
      * This will work for both http, https with or without www
      * @type String
      */
-    
     //attach trailing slash to both foldernames
     var devFolder = devFolderName ? devFolderName+"/" : "";
+
     var prodFolder = prodFolderName ? prodFolderName+"/" : "";
     
     if((hostname.search("localhost") !== -1) || (hostname.search("192.168.") !== -1)  || (hostname.search("127.0.0.") !== -1)){
-        return window.location.origin+"/"+devFolder;
+        return devFolder;
     }
     
     else{
-        return window.location.origin+"/"+prodFolder;
+        return prodFolder + "/";
     }
 }
