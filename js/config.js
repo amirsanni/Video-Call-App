bd9ed45599986f4512d2bdaee5303ac967ac4115
Amir Sanni <amirsanni@gmail.com>
2020-04-13 19:51:35 +0100

    remove config.js

--- js/config.js
+++ js/config.js
@@ -6,31 +6,4 @@
 'use strict';
 
 
-const appRoot = setAppRoot('video-call-app', 'video-call-app');
-const spinnerClass = 'fa fa-spinner faa-spin animated';
-
-function setAppRoot(devFolderName, prodFolderName){
-    var hostname = window.location.hostname;
-
-    /*
-     * set the appRoot
-     * This will work for both http, https with or without www
-     * @type String
-     */
-    
-    //attach trailing slash to both foldernames
-    var devFolder = devFolderName ? devFolderName+"/" : "";
-    var prodFolder = prodFolderName ? prodFolderName+"/" : "";
-    
-    var appRoot = "";
-    
-    if(hostname === "localhost" || (hostname.search("192.168.") !== -1)  || (hostname.search("127.0.0.") !== -1)){
-        appRoot = window.location.origin+"/"+devFolder;
-    }
-    
-    else{
-        appRoot = window.location.origin+"/"+prodFolder;
-    }
-    
-    return appRoot;
-}
+const appRoot = 'http://localhost:8888';
