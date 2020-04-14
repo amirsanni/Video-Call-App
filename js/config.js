df0b0602e2d996d1aa75c13d7e90108db72e67ae
Amir Sanni <amirsanni@gmail.com>
2020-04-14 13:00:31 +0100

    Add missing variable

--- js/config.js
+++ js/config.js
@@ -8,3 +8,4 @@
 
 const appRoot = 'http://localhost:8888/';
 const wsUrl = 'ws://localhost:8080';//use wss://localhost:8080/comm for secured connection
+const spinnerClass = 'fa fa-spinner faa-spin animated';
